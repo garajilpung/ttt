d370a6669a03aabcf311e762ed534a01fef3bdf6
ksos73 <ksos73@nate.com>
2023-02-14 19:39:33 +0900

    first

---
+++ src/components/Board.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Square from './Square'
+import "./Board.css";
+
+
+
+
+const Board = ({ squares, onClick}) => {
+  
+  const renderSqure = (i) => {
+    return <Square value={ squares[i]} onClick={() => { onClick(i)}}/>
+  }
+  
+  return (
+    <div className='board-wrapper'>
+      <div className='board-row'>
+        {renderSqure(0)}
+        {renderSqure(1)}
+        {renderSqure(2)}
+      </div>
+
+      <div className=''>
+        {renderSqure(3)}
+        {renderSqure(4)}
+        {renderSqure(5)}
+      </div>
+
+      <div className=''>
+        {renderSqure(6)}
+        {renderSqure(7)}
+        {renderSqure(8)}
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Board
